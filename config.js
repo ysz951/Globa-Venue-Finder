1135d4976cd7911423d01c0488b8a06916a3426f
ysz951 <shengyang_zhou@baylor.edu>
2020-06-04 17:52:39 -0500

    h

--- config.js
+++
@@ -1,6 +0,0 @@
-// google apikey
-const key = 'AIzaSyDal57aUyQAAoSUb82ptXKbH1X4M-iYu8I';
-
-// foursquare client information
-const client_id = 'K3JLT3HH0JYERCWAZQYHLHKFMX4JFM5VQ4FXUOOLUQIRWPZ2';
-const client_secret = 'NKTAY0IR52WOKLSWDY32SR5AYVKG2RAQIEZK3NENASVTAB44';
